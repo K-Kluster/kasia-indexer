c11cb1cc21eb3bd0f23287f7ab1540db39d005f5
143672 <biryukov.maxim.vladimirovich@yandex.ru>
2025-07-12 17:31:36 +0400

    contextual message processing

--- indexer-lib/src/block_worker.rs
+++ indexer-lib/src/block_worker.rs
@@ -1,4 +1,6 @@
-use crate::database::resolution_keys::HandshakeKeyForResolution;
+use crate::database::resolution_keys::{
+    ContextualMessageKeyForResolution, HandshakeKeyForResolution,
+};
 use crate::database::skip_tx::SkipTxPartition;
 use crate::{
     BlockOrMany,
@@ -6,19 +8,17 @@ use crate::{
         acceptance::{AcceptanceToTxIDPartition, TxIDToAcceptancePartition},
         contextual_message_by_sender::ContextualMessageBySenderPartition,
         handshake::{
-            AddressPayload, HandshakeByReceiverPartition, HandshakeBySenderPartition,
-            HandshakeKeyByReceiver, TxIdToHandshakePartition,
+            AddressPayload, HandshakeByReceiverPartition, HandshakeKeyByReceiver,
+            TxIdToHandshakePartition,
         },
         metadata::MetadataPartition,
-        pending_sender_resolution::PendingSenderResolutionPartition,
-        unknown_accepting_daa::UnknownAcceptingDaaPartition,
     },
     fifo_set::FifoSet,
     historical_syncer::Cursor,
 };
 use fjall::TxKeyspace;
 use kaspa_consensus_core::tx::Transaction;
-use kaspa_rpc_core::{RpcHash, RpcTransactionId};
+use kaspa_rpc_core::RpcHash;
 use protocol::operation::SealedOperation;
 use protocol::operation::deserializer::parse_sealed_operation;
 use tracing::info;
@@ -30,20 +30,15 @@ pub struct BlockWorker {
 
     tx_keyspace: TxKeyspace,
 
-    // Core partitions
     metadata_partition: MetadataPartition,
-    handshake_partition_by_sender: HandshakeBySenderPartition,
+
     handshake_by_receiver_partition: HandshakeByReceiverPartition,
     tx_id_to_handshake_partition: TxIdToHandshakePartition,
 
     contextual_message_partition: ContextualMessageBySenderPartition,
 
-    // acceptance
     acceptance_to_tx_id_partition: AcceptanceToTxIDPartition,
     tx_id_to_acceptance_partition: TxIDToAcceptancePartition,
-    // Resolution partitions
-    unknown_daa_partition: UnknownAcceptingDaaPartition,
-    pending_sender_partition: PendingSenderResolutionPartition,
 
     skip_tx_partition: SkipTxPartition,
 }
@@ -121,8 +116,38 @@ impl BlockWorker {
                                         self.acceptance_to_tx_id_partition
                                             .insert_wtx(&mut wtx, tx_id.as_bytes());
                                     }
-                                    SealedOperation::ContextualMessageV1(_) => {
-                                        todo!()
+                                    SealedOperation::ContextualMessageV1(op) => {
+                                        self.contextual_message_partition.insert(
+                                            &mut wtx,
+                                            Default::default(),
+                                            &op.alias,
+                                            b.header.timestamp,
+                                            b.header.hash.as_bytes(),
+                                            1,
+                                            tx_id.as_bytes(),
+                                            &op.sealed_hex,
+                                        )?;
+                                        self.tx_id_to_acceptance_partition
+                                            .insert_contextual_message_wtx(
+                                                &mut wtx,
+                                                tx_id.as_bytes(),
+                                                &ContextualMessageKeyForResolution {
+                                                    alias: {
+                                                        let mut alias = [0u8; 16];
+                                                        alias.copy_from_slice(
+                                                            &op.alias[..op.alias.len().min(16)],
+                                                        );
+                                                        alias
+                                                    },
+                                                    block_time: b.header.timestamp.to_be_bytes(),
+                                                    block_hash: b.header.hash.as_bytes(),
+                                                    version: 1,
+                                                    tx_id: tx_id.as_bytes(),
+                                                    attempt_count: 0,
+                                                },
+                                            );
+                                        self.acceptance_to_tx_id_partition
+                                            .insert_wtx(&mut wtx, tx_id.as_bytes());
                                     }
                                     SealedOperation::PaymentV1(_) => {
                                         todo!()
@@ -137,7 +162,7 @@ impl BlockWorker {
                                 hash: b.header.hash,
                             },
                         )?;
-
+                        wtx.commit()??;
                         Ok(())
                     })?;
                 }
