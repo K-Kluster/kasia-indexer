45169f6d1e76f0cd46699ad326578d4698ae19b0
IzioDev <9900846+IzioDev@users.noreply.github.com>
2025-08-25 19:41:45 +0200

    refacto(signals): dedicated mod

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -44,6 +44,7 @@ use workflow_core::channel::Channel;
 mod api;
 mod config;
 mod context;
+mod signals;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -278,7 +279,7 @@ async fn main() -> anyhow::Result<()> {
         ticker: shutdown_ticker_tx,
     };
     tokio::select! {
-        _ = wait_for_signal_impl() => {
+        _ = signals::wait_for_signal_impl() => {
             info!("Termination signal received. Shutting down...");
             shutdown.shutdown(None).await
         },
@@ -436,40 +437,4 @@ impl Shutdown {
             }
         }
     }
-}
-
-/// Waits for a signal that requests a graceful shutdown, like SIGTERM or SIGINT.
-#[cfg(unix)]
-async fn wait_for_signal_impl() {
-    use tokio::signal::unix::{SignalKind, signal};
-
-    // Infos here:
-    // https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
-    let mut signal_terminate = signal(SignalKind::terminate()).unwrap();
-    let mut signal_interrupt = signal(SignalKind::interrupt()).unwrap();
-
-    tokio::select! {
-        _ = signal_terminate.recv() => tracing::debug!("Received SIGTERM."),
-        _ = signal_interrupt.recv() => tracing::debug!("Received SIGINT."),
-    };
-}
-
-/// Waits for a signal that requests a graceful shutdown, Ctrl-C (SIGINT).
-#[cfg(windows)]
-async fn wait_for_signal_impl() {
-    use tokio::signal::windows;
-
-    // Infos here:
-    // https://learn.microsoft.com/en-us/windows/console/handlerroutine
-    let mut signal_c = windows::ctrl_c().unwrap();
-    let mut signal_break = windows::ctrl_break().unwrap();
-    let mut signal_close = windows::ctrl_close().unwrap();
-    let mut signal_shutdown = windows::ctrl_shutdown().unwrap();
-
-    tokio::select! {
-        _ = signal_c.recv() => tracing::debug!("Received CTRL_C."),
-        _ = signal_break.recv() => tracing::debug!("Received CTRL_BREAK."),
-        _ = signal_close.recv() => tracing::debug!("Received CTRL_CLOSE."),
-        _ = signal_shutdown.recv() => tracing::debug!("Received CTRL_SHUTDOWN."),
-    };
 }
