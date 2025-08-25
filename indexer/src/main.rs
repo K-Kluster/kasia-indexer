77c36adde7cadb2cdc2e503d4eed3ab6bbd09f1c
IzioDev <9900846+IzioDev@users.noreply.github.com>
2025-08-25 19:02:32 +0200

    fix(signals): SIGTERM

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -57,6 +57,8 @@ async fn main() -> anyhow::Result<()> {
 
     let _g = init_logs(&context)?;
 
+    info!("Using DB Path: {}", context.db_path.to_string_lossy());
+
     let config = Config::new(context.clone().db_path).max_write_buffer_size(512 * 1024 * 1024);
     let tx_keyspace = config.open_transactional()?;
     let virtual_daa = Arc::new(AtomicU64::new(0));
@@ -276,10 +278,10 @@ async fn main() -> anyhow::Result<()> {
         ticker: shutdown_ticker_tx,
     };
     tokio::select! {
-        _ = tokio::signal::ctrl_c() => {
+        _ = wait_for_signal_impl() => {
             info!("Termination signal received. Shutting down...");
             shutdown.shutdown(None).await
-        }
+        },
         r = api_handle => {
            _ = r.inspect(|_| info!("api has stopped"))
                 .inspect_err(|err| error!("api has stopped with error: {}", err));
@@ -434,4 +436,40 @@ impl Shutdown {
             }
         }
     }
+}
+
+/// Waits for a signal that requests a graceful shutdown, like SIGTERM or SIGINT.
+#[cfg(unix)]
+async fn wait_for_signal_impl() {
+    use tokio::signal::unix::{SignalKind, signal};
+
+    // Infos here:
+    // https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
+    let mut signal_terminate = signal(SignalKind::terminate()).unwrap();
+    let mut signal_interrupt = signal(SignalKind::interrupt()).unwrap();
+
+    tokio::select! {
+        _ = signal_terminate.recv() => tracing::debug!("Received SIGTERM."),
+        _ = signal_interrupt.recv() => tracing::debug!("Received SIGINT."),
+    };
+}
+
+/// Waits for a signal that requests a graceful shutdown, Ctrl-C (SIGINT).
+#[cfg(windows)]
+async fn wait_for_signal_impl() {
+    use tokio::signal::windows;
+
+    // Infos here:
+    // https://learn.microsoft.com/en-us/windows/console/handlerroutine
+    let mut signal_c = windows::ctrl_c().unwrap();
+    let mut signal_break = windows::ctrl_break().unwrap();
+    let mut signal_close = windows::ctrl_close().unwrap();
+    let mut signal_shutdown = windows::ctrl_shutdown().unwrap();
+
+    tokio::select! {
+        _ = signal_c.recv() => tracing::debug!("Received CTRL_C."),
+        _ = signal_break.recv() => tracing::debug!("Received CTRL_BREAK."),
+        _ = signal_close.recv() => tracing::debug!("Received CTRL_CLOSE."),
+        _ = signal_shutdown.recv() => tracing::debug!("Received CTRL_SHUTDOWN."),
+    };
 }
